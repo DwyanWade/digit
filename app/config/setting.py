48bf2f71e3eb766502b5972e9370017e4af20dd8
pengjianqiang <18820121479@163.com>
2018-10-01 04:48:14 +0800

    ss

---
+++ app/config/setting.py
@@ -0,0 +1,11 @@
+"""
+Created on 2018/6/6 0:43
+
+"""
+__Author__ = '阿强'
+
+TOKEN_EXPIRATION = 2*3600
+IMG_ROOT_URL = "http://127.0.0.1:5000/static/images"
+WX_APPID = "wxf5116439f6febd83"
+APP_SECRET = "590949cb6aba9b6f884dbaf895d4035e"
+LOGIN_URL = "https://api.weixin.qq.com/sns/jscode2session?appid={}&secret={}&js_code={}&grant_type=authorization_code"
